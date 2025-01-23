b33f3f96778a5467e3e277be00fcc618c97b3dd6
Yashindi Bhagya <136606727+yashindibhagya@users.noreply.github.com>
2025-01-23 20:54:28 +0530

    Add files via upload

---
+++ Bird Game/Assets/PipeMoveScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PipeMoveScript : MonoBehaviour
+{
+
+    public float moveSpeed = 5;
+    public float deadZone = -45;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        
+        if (transform.position.x < deadZone)
+        {
+            Debug.Log("Pipe Deleted");
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Bird Game/Assets/birdsprite.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class birdsprite : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive == true)
+        {
+            myRigidbody.linearVelocity = Vector2.up * flapStrength;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+}
